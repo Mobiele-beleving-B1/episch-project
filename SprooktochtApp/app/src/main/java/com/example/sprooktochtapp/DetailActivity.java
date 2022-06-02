77879df27268c3b4a5c6248c47935f213fbad77c
Oliver Norton <o.k.norton21@gmail.com>
2022-06-02 13:50:53 +0200

    Added interaction between the fairyTaleManager and the Detail activity

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/DetailActivity.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/DetailActivity.java
@@ -2,12 +2,27 @@ package com.example.sprooktochtapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView sprookjeName;
+    TextView sprookjeDescription;
+    ImageView sprookjeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        FairyTaleManager.createFairyTales();
+
+        sprookjeName = (TextView) findViewById(R.id.sprookjeName);
+        sprookjeDescription = (TextView) findViewById(R.id.sprookjeDescription);
+        sprookjeImage = (ImageView) findViewById(R.id.sprookjeImage);
+
+        FairyTale selectedFairyTale = FairyTaleManager.getFairyTale(1);
+
+        sprookjeName.setText(selectedFairyTale.getNameOfTale());
+        sprookjeDescription.setText(selectedFairyTale.getTaleDescription());
     }
 }
