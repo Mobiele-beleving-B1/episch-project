5776853eb88263aefc8b4ac40e2dc4b3ec9cce33
Oliver Norton <o.k.norton21@gmail.com>
2022-06-02 14:14:36 +0200

    Added image id finding
    Currently not functional

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/DetailActivity.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/DetailActivity.java
@@ -1,14 +1,15 @@
 package com.example.sprooktochtapp;
 
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-    TextView sprookjeName;
-    TextView sprookjeDescription;
-    ImageView sprookjeImage;
+    TextView fairyTaleName;
+    TextView fairyTaleDescription;
+    ImageView fairyTaleImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +17,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         FairyTaleManager.createFairyTales();
 
-        sprookjeName = (TextView) findViewById(R.id.sprookjeName);
-        sprookjeDescription = (TextView) findViewById(R.id.sprookjeDescription);
-        sprookjeImage = (ImageView) findViewById(R.id.sprookjeImage);
+        fairyTaleName = (TextView) findViewById(R.id.fairyTaleName);
+        fairyTaleDescription = (TextView) findViewById(R.id.fairyTaleDescription);
+        fairyTaleImage = (ImageView) findViewById(R.id.fairyTaleImage);
 
         FairyTale selectedFairyTale = FairyTaleManager.getFairyTale(1);
 
-        sprookjeName.setText(selectedFairyTale.getNameOfTale());
-        sprookjeDescription.setText(selectedFairyTale.getTaleDescription());
+        fairyTaleName.setText(selectedFairyTale.getNameOfTale());
+        fairyTaleDescription.setText(selectedFairyTale.getTaleDescription());
+//        fairyTaleImage.setImageDrawable(ResourcesCompat.getDrawable(getResources(), selectedFairyTale.getImageOfTaleId(), null));
     }
 }
