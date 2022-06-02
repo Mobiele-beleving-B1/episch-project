b710c97019c1434eabc3bcbbb6da2e48673baeb8
Oliver Norton <o.k.norton21@gmail.com>
2022-06-02 10:20:52 +0200

    Added detail activity layout and adjusted main activity
    Added the layout for the detail activity and a working button in the toolbar to bring the user back. Also edited main page constraints.

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/DetailActivity.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/DetailActivity.java
@@ -1,13 +1,37 @@
 package com.example.sprooktochtapp;
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
-
+    ImageView sprookjeImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        sprookjeImage = (ImageView) findViewById(R.id.homeImage);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
