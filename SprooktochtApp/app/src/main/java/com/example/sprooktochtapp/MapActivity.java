5761d32066b0315a74257951facbe1013405cf2c
Oliver Norton <o.k.norton21@gmail.com>
2022-06-07 14:43:52 +0200

    Base structure for nfc reading added
    Added manifest logs that allow nfc reading. Also added nfc reading methods to the map activity

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/MapActivity.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/MapActivity.java
@@ -1,13 +1,49 @@
 package com.example.sprooktochtapp;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MapActivity extends AppCompatActivity {
+
+    //nfc initialization
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    final static String TAG = "nfc testing";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        //set adapter
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        //error message in case of no nfc adapter
+        if(nfcAdapter == null){
+            Toast.makeText(this, "Nfc reader not active", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        //pending intent creation
+        pendingIntent = PendingIntent.getActivity(this,0,new Intent(this,this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
+    }
+
+    //allows constant reading for nfc's
+    @Override
+    protected void onResume() {
+        super.onResume();
+        assert nfcAdapter != null;
+        nfcAdapter.enableForegroundDispatch(this,pendingIntent,null,null);
+    }
+
+    //pauses constant reading for nfc's
+    protected void onPause() {
+        super.onPause();
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
     }
 }
