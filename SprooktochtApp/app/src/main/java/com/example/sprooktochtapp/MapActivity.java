fcde0f67819f136b90d9abdc0d286ae7f3a1ecfe
Oliver Norton <o.k.norton21@gmail.com>
2022-06-09 14:09:20 +0200

    Added extra styling to map_activity and edited contsraints
    Added styling to map_activity and changed the constraints on the existing border style elements to resize to any screen size.

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/MapActivity.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/MapActivity.java
@@ -1,13 +1,20 @@
 package com.example.sprooktochtapp;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class MapActivity extends AppCompatActivity {
+    ImageView bottomHomeImage;
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        bottomHomeImage = (ImageView) findViewById(R.drawable.john_lennon);
+
     }
 }
