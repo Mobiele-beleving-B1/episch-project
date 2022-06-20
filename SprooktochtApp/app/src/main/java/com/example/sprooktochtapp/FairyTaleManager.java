2f19c8d3c0c8b8b408c56f5f58a20aa1f396fcc6
Stijn Custers <stijn657@gmail.com>
2022-06-20 20:54:16 +0200

    redirected strings asnd images to the right files
    ergtgy

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/FairyTaleManager.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/FairyTaleManager.java
@@ -14,8 +14,8 @@ public class FairyTaleManager {
     }
 
     public static void createFairyTales() {
-        fairyTales.add(new FairyTale("De wolf en de drie biggetjes", R.drawable.de_drie_biggetjes, "unknown", "placeholder game problem", App.getAppResources().getString(R.string.drie_biggetjes)));
-        fairyTales.add(new FairyTale("Hans en Grietje", R.drawable.hansengrietje, "unknown", "placeholder game problem", "Hans en grietje raken verdwaald in een bos en komen bij een huisje van snoep en snoepen ervan. Er blijkt een heks te wonen, die hen eerst vriendelijk uitnodigt om binnen te komen. Dan sluit ze Hans op om hem vet te mesten en op te eten. Grietje moet helpen in de huishouding. Hans weet de heks voor de gek te houden door steeds een afgekloven botje uit zijn kooi te steken wanneer de heks wil voelen hij dik hij al is. De heks heeft niks door want ze is blind. Uiteindelijk heeft de heks er genoeg van en ze besluit Hans toch op te eten. Wanneer Grietje de oven moet aanmaken waar haar broertje in gebraden zal worden, lukt het haar de heks zelf in de oven te duwen: als Grietjes van de heks in de oven moet voelen of het al heet genoeg is, zegt ze dat de oven te klein is. De domme heks gaat hierop voor de oven staan om te laten zien dat er zoveel ruimte is dat zij er zelf zelfs in past."));
+        fairyTales.add(new FairyTale("De drie biggetjes", R.drawable.de_drie_biggetjes_uitleg, "unknown", "placeholder game problem", App.getAppResources().getString(R.string.drieBiggetjesHulp)));
+        fairyTales.add(new FairyTale("Hans en Grietje", R.drawable.hans_en_grietje_uitleg, "unknown", "placeholder game problem", App.getAppResources().getString(R.string.hansEnGrietjeHulp)));
     }
 
     public static ArrayList<FairyTale> getFairyTales() {
