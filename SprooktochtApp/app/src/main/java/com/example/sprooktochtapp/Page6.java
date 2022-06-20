fc17c300844b9aa37874a8de6599ce750249dc12
Stijn Custers <stijn657@gmail.com>
2022-06-20 23:33:26 +0200

    added translations for Strings in the XML file

--- SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/Page6.java
+++ SprooktochtApp/app/src/main/java/com/example/sprooktochtapp/Page6.java
@@ -5,17 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class Page6 extends Fragment {
 
     public Page6() {
-        // Required empty public constructor
+
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
